b3d537957501e17e79dd5a109b9fc091ae2d316e
Vixiea <70721547+Vixiea@users.noreply.github.com>
2023-04-19 02:24:15 -0500

    Update discord_events_handlers.rb
    Update for chat support.

--- lib/discord_events_handlers.rb
+++ lib/discord_events_handlers.rb
@@ -6,34 +6,36 @@ module ::DiscordBot::DiscordEventsHandlers
     message do |event|
 
       return if !SiteSetting.discord_bot_auto_channel_sync && SiteSetting.discord_bot_discourse_announcement_topic_id.blank? && (event.message.channel.id != SiteSetting.discord_bot_announcement_channel_id)
+      return if event.message.author.bot = true
 
       system_user = User.find_by(id: -1)
 
-      associated_user = UserAssociatedAccount.find_by(provider_uid: event.message.author.id)
+      associated_user = UserCustomField.find_by(value: event.message.auther.name)
+
       unless associated_user.nil?
-        posting_user = User.find_by(id: associated_user.user_id)
+        message_user = User.find_by(id: associated_user.user_id)
       else
-        posting_user = system_user
+        message_user = system_user
+        raw = event.message.nick.to_s + ": "
       end
 
-      raw = event.message.to_s
-      if !raw.blank?
+      discordmessage = event.message.to_s
+      raw += discordmessage
+      if !discordmessage.blank?
         if SiteSetting.discord_bot_auto_channel_sync
-          matching_category = Category.find_by(name: event.message.channel.name)
-          unless matching_category.nil?
-            if !(target_topic = Topic.find_by(title: I18n.t("discord_bot.discord_events.auto_message_copy.default_topic_title", channel_name: matching_category.name))).nil?
-              new_post = PostCreator.create!(posting_user, raw: raw, topic_id: target_topic.id, skip_validations: true)
-            else
-              new_post = PostCreator.create!(posting_user, title: I18n.t("discord_bot.discord_events.auto_message_copy.default_topic_title", channel_name: matching_category.name), raw: raw, category: matching_category.id, skip_validations: true)
-            end
+          matching_channel = Chat::Channel.find_by(name: event.message.channel.name)
+          unless matching_channel.nil?
+            Chat::MessageCreator.create(chat_channel: matching_channel, user: message_user, content: raw).chat_message
+            @@DiscordPost = 1
             return
           end
         end
-        if !SiteSetting.discord_bot_discourse_announcement_topic_id.blank? && (event.message.channel.id == SiteSetting.discord_bot_announcement_channel_id.to_i)
-          # Copy the message to the assigned Discourse announcement Topic if assigned in plugin settings
-          discourse_announcement_topic = Topic.find_by(id: SiteSetting.discord_bot_discourse_announcement_topic_id.to_i)
-          unless discourse_announcement_topic.nil?
-            new_post = PostCreator.create!(posting_user, raw: raw, topic_id: discourse_announcement_topic.id, skip_validations: true)
+        if !SiteSetting.discord_bot_chat_listening_categories.blank?
+          chat_listening_categories = SiteSetting.discord_bot_chat_listening_categories.split('|')
+          matching_channel = Chat::Channel.find_by(name: event.message.channel.name)
+          if chat_listening_categories.include?(matching_channel.to_s) then
+            Chat::MessageCreator.create(chat_channel: matching_channel, user: message_user, content: raw).chat_message
+            @@DiscordPost = 1
           end
         end
       end
