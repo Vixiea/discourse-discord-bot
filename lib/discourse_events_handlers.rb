b459f6f4c868d172a505a278855f8376e2debae5
Vixiea <70721547+Vixiea@users.noreply.github.com>
2023-04-19 10:24:51 -0500

    Update discourse_events_handlers.rb
    Removed break statement.

--- lib/discourse_events_handlers.rb
+++ lib/discourse_events_handlers.rb
@@ -3,24 +3,21 @@ module ::DiscordBot::DiscourseEventsHandlers
   def self.hook_events
     DiscourseEvent.on(:chat_message_created) do |message|
       if Chat::Channel.find_by(id: message.chat_channel_id).chatable_type != 'DirectMessage' && !::DiscordBot::Bot.discord_bot.nil? then
-          if $DiscordPost = 1 then
-            $DiscordPost = 0
-            break
-          end
           chat_listening_categories = SiteSetting.discord_bot_chat_listening_categories.split('|')
           matching_channel = Chat::Channel.find_by(id: message.chat_channel_id)
-          if SiteSetting.discord_bot_auto_channel_sync then
+          if SiteSetting.discord_bot_auto_channel_sync- && $DiscordPost = 0 then
             channel_id = matching_channel.description.to_s
             #channel_id = matching_channel.slug
             text = User.find_by(id: message.user_id).name + ": " + message.message.to_s
             ::DiscordBot::Bot.discord_bot.send_message(channel_id, text)
         else
-          if chat_listening_categories.include?(matching_channel.to_s) then
+          if chat_listening_categories.include?(matching_channel.to_s) && DiscordPost = 0 then
             channel_id = matching_channel.description.to_s
             #channel_id = matching_channel.slug
             text = User.find_by(id: message.user_id).name + ": " + message.message.to_s 
             ::DiscordBot::Bot.discord_bot.send_message(channel_id, message)
           end
+          $DiscordPost = 0
         end
       end
     end
