1add212467dafc8394b840cead8e01fae68ea8c5
Vixiea <70721547+Vixiea@users.noreply.github.com>
2023-04-19 10:14:29 -0500

    Update discourse_events_handlers.rb
    Channel slugs wont work for posting to Discord, id is required.

--- lib/discourse_events_handlers.rb
+++ lib/discourse_events_handlers.rb
@@ -10,14 +10,14 @@ module ::DiscordBot::DiscourseEventsHandlers
           chat_listening_categories = SiteSetting.discord_bot_chat_listening_categories.split('|')
           matching_channel = Chat::Channel.find_by(id: message.chat_channel_id)
           if SiteSetting.discord_bot_auto_channel_sync then
-            #channel_id = matching_channel.description.to_s
-            channel_id = matching_channel.slug
+            channel_id = matching_channel.description.to_s
+            #channel_id = matching_channel.slug
             text = User.find_by(id: message.user_id).name + ": " + message.message.to_s
             ::DiscordBot::Bot.discord_bot.send_message(channel_id, text)
         else
           if chat_listening_categories.include?(matching_channel.to_s) then
-            #channel_id = matching_channel.description.to_s
-            channel_id = matching_channel.slug
+            channel_id = matching_channel.description.to_s
+            #channel_id = matching_channel.slug
             text = User.find_by(id: message.user_id).name + ": " + message.message.to_s 
             ::DiscordBot::Bot.discord_bot.send_message(channel_id, message)
           end
