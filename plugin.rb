830db4181a2d139dc005d5ed6887a2e81cdb5748
Vixiea <70721547+Vixiea@users.noreply.github.com>
2023-05-12 13:43:44 -0500

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -17,8 +17,8 @@ gem 'http-cookie', '1.0.3'
 gem 'http-accept', '1.7.0', { require: false }
 gem 'rest-client', '2.1.0.rc1'
 
-gem 'discordrb-webhooks', '3.4.2', { require: false }
-gem 'discordrb', '3.4.0'
+gem 'discordrb-webhooks'
+gem 'discordrb'
 
 require 'discordrb'
 require 'discordrb/webhooks'
