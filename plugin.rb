b87293839220966dc14d5078df3b6e38cd49f1ee
Vixiea <70721547+Vixiea@users.noreply.github.com>
2023-05-15 13:30:13 -0500

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -6,8 +6,8 @@
 # url: https://github.com/merefield/discourse-discord-bot
 
 gem 'event_emitter', '0.2.6'
-gem 'websocket', '1.2.8'
-gem 'websocket-client-simple', '0.3.0'
+gem 'websocket', '1.2.9'
+gem 'websocket-client-simple', '0.6.0'
 gem 'opus-ruby', '1.0.1', { require: false }
 gem 'netrc', '0.11.0'
 gem 'mime-types-data', '3.2019.1009'
@@ -15,11 +15,7 @@ gem 'mime-types', '3.3.1'
 gem 'domain_name', '0.5.20180417'
 gem 'http-cookie', '1.0.3'
 gem 'http-accept', '1.7.0', { require: false }
-gem 'rest-client', '2.1.0.rc1'
-git 'https://github.com/shardlab/discordrb.git' do
-  gem 'discordrb'
-  gem 'discordrb-webhooks'
-end
+gem 'rest-client', '2.1.0'
 
 require 'discordrb'
 require 'discordrb/webhooks'
